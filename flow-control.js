2f88172890617f78adf3f4b1a6fc533ac53d9623
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-21 12:26:34 -0500

    Broken for testing

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,25 @@
 function basicTeenager(age) {
-
+  if (age > 12 && age < 20){
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (age > 12 && age < 20){
+    return "You are a teenager"
+  }else{
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (age > 12 && age < 20){
+    return "You are a teenager!"
+  }elseif(age > 12){
+    return "You are not kid"
+  }else{
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
